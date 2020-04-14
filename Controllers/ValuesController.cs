8e3c3452317c07b901187ad83f44466a0de8ad38
Saptarshi152080 <63588948+Saptarshi152080@users.noreply.github.com>
2020-04-14 14:18:33 +0530

    First Commit

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace DatingApp.API.Controllers
@@ -19,16 +20,16 @@ namespace DatingApp.API.Controllers
         }
 
         [HttpGet]
-        public IActionResult Getvalues()
+        public async Task<IActionResult> Getvalues()
         {
-            var values = _context.Values.ToList();
+            var values = await _context.Values.ToListAsync();
             return Ok(values);
         }
 
         [HttpGet("{id}")]
-        public IActionResult Getvalue(int Id)
+        public async Task<IActionResult> Getvalue(int Id)
         {
-            var value = _context.Values.FirstOrDefault(x => x.Id == Id);
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == Id);
             return Ok(value);
         }
     }
